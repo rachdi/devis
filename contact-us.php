58335537169608e958f07d5218641d1839974313
rachdi <bigking3100@gmail.com>
2017-03-05 00:22:18 +0100

    difj

--- contact-us.php
+++ contact-us.php
@@ -9,19 +9,14 @@ $_SESSION['groupe3'];
 $_SESSION['groupe4'];
 $_POST['email'];
 
-if (isset($_POST['submit'])) {
-	$msg= $_SESSION['group1'].'\n'
-	.$_SESSION['group2'].'\n'
-	.$_SESSION['groupe3'].'\n'
-	.$_SESSION['groupe4'].'\n'
-	.$_POST['email'];
-	mail('bassemrachdi1@gmail.com', 'Devis', $msg);
-	echo "Votre msg est bien envoyer vous recevez un devis bientot <a href='page1_form.php'>click ici</a> pour refaire un autre devis" ;
-}else
-{
-	header('location: failcontact.php');
-	exit(0);
-}
+
+$msg= $_SESSION['group1'].'\n'
+.$_SESSION['group2'].'\n'
+.$_SESSION['groupe3'].'\n'
+.$_SESSION['groupe4'].'\n'
+.$_POST['email'];
+mail('bassemrachdi1@gmail.com', 'Devis', $msg);
+echo "Votre msg est bien envoyer vous recevez un devis bientot <a href='page1_form.php'>click ici</a> pour refaire un autre devis" ;
 
 
 
